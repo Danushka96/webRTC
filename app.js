d1008246567260e075c52f2c3c7cbbeb1fecfc6e
danushka96 <danushkaherath96@gmail.com>
2020-03-15 21:51:52 +0530

    added basic video transfer implementation

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+app.use('/scripts', express.static(`${__dirname}/node_modules/`));
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
